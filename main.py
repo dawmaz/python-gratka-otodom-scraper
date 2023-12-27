957f3ff3dd4342f2ef9530713d3fca4945766ec8
Dawid Mazur <dawmaz@gmail.com>
2023-12-27 14:03:15 +0100

    Added otodom to main program

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 import time
 
 from gratka.gratka_cyclic_schedulers import consume_scheduled_jobs, consume_single_offer, consume_images, queue_defined_jobs
+from otodom import otodom_cyclic_schedulers
 
 
 def defined_jobs():
@@ -20,19 +21,52 @@ def process_images():
     return threading.Thread(target=consume_images, args=())
 
 
+def defined_jobs_otodom():
+    return threading.Thread(target=otodom_cyclic_schedulers.queue_defined_jobs, args=())
+
+
+def process_defined_jobs_otodom():
+    return threading.Thread(target=otodom_cyclic_schedulers.consume_scheduled_jobs, args=())
+
+
+def process_single_offer_otodom():
+    return threading.Thread(target=otodom_cyclic_schedulers.consume_single_offer, args=())
+
+
+def process_images_otodom():
+    return threading.Thread(target=otodom_cyclic_schedulers.consume_images, args=())
+
+
 def thread_orchestrator(threads_dict):
     while True:
         print(f'Checking if threads are alive')
         for key, thread in threads_dict.items():
             if not thread.is_alive():
                 if 'process_single_offer' in key:
-                    new_thread = process_single_offer()
+                    if 'otodom' in key:
+                        new_thread = process_single_offer_otodom()
+                    else:
+                        new_thread = process_single_offer()
                 elif 'process_images' in key:
-                    new_thread = process_images()
+                    if 'otodom' in key:
+                        new_thread = process_images_otodom()
+                    else:
+                        new_thread = process_single_offer()
+                elif 'process_images' in key:
+                    if 'otodom' in key:
+                        new_thread = process_images_otodom()
+                    else:
+                        new_thread = process_images()
                 elif key == 'defined_jobs':
-                    new_thread = defined_jobs()
+                    if 'otodom' in key:
+                        new_thread = defined_jobs_otodom()
+                    else:
+                        new_thread = defined_jobs()
                 elif key == 'process_defined_jobs':
-                    new_thread = process_defined_jobs()
+                    if 'otodom' in key:
+                        new_thread = process_defined_jobs_otodom()
+                    else:
+                        new_thread = process_defined_jobs()
 
                 threads_dict[key] = new_thread
                 new_thread.start()
@@ -41,20 +75,25 @@ def thread_orchestrator(threads_dict):
         time.sleep(30)
 
 
-
 def run_extra_threads():
     threads = {}
     defined_jobs_thread = defined_jobs()
+    defined_jobs_thread_otodom = defined_jobs_otodom()
+
     threads['defined_jobs'] = defined_jobs_thread
+    threads['defined_jobs_otodom'] = defined_jobs_thread_otodom
 
     process_defined_jobs_thread = process_defined_jobs()
+    process_defined_jobs_thread_otodom = process_defined_jobs_otodom()
+
     threads['process_defined_jobs'] = process_defined_jobs_thread
+    threads['process_defined_jobs_otodom'] = process_defined_jobs_thread_otodom
 
     for i in range(8):
-        x = process_single_offer()
-        threads[f'process_single_offer_{i}'] = x
-        y = process_images()
-        threads[f'process_images_{i}'] = y
+        threads[f'process_single_offer_{i}'] = process_single_offer()
+        threads[f'process_single_offer_otodom_{i}'] = process_single_offer_otodom()
+        threads[f'process_images_{i}'] = process_images()
+        threads[f'process_images_otodom_{i}'] = process_images_otodom()
 
     for thread in threads.values():
         thread.start()
