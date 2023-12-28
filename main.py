ca4e81d2cda862a0477d81cc280a1b455e0b7da3
Dawid Mazur <dawmaz@gmail.com>
2023-12-28 14:01:10 +0100

    Fixes to thread orchestrator, should now reborn the consumers

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import threading
 import time
+import requests
 
-from gratka.gratka_cyclic_schedulers import consume_scheduled_jobs, consume_single_offer, consume_images, queue_defined_jobs
+from gratka.gratka_cyclic_schedulers import consume_scheduled_jobs, consume_single_offer, consume_images, \
+    queue_defined_jobs
 from otodom import otodom_cyclic_schedulers
 
 
@@ -72,22 +74,60 @@ def thread_orchestrator(threads_dict):
                 new_thread.start()
                 print(f'Thread {key} is dead. New one was created')
 
+        consumers = get_alive_consumers()
+
+        if consumers:
+            to_reborn = check_consumers_alive(consumers, 'otodom_process_scheduled_jobs', 'process_scheduled_jobs')
+            reborn_threads(threads_dict, to_reborn)
+
         time.sleep(30)
+
+
+def get_alive_consumers():
+    api_url = f'http://localhost:15672/api/consumers'
+    response = requests.get(api_url, auth=('guest', 'guest'))
+
+    if response.status_code == 200:
+        consumers = response.json()
+        return consumers
+    else:
+        print(f"Failed to retrieve consumers. Status code: {response.status_code}")
+        return None
+
+
+def check_consumers_alive(consumer, *consumer_names_to_check):
+    return [name for name in consumer_names_to_check if all(name != c['queue']['name'] for c in consumer)]
+
+
+def reborn_threads(threads_dict, to_reborn):
+    if to_reborn:
+        for name in to_reborn:
+            new_thread = None
+            if 'otodom_process_scheduled_jobs' in name:
+                new_thread = process_defined_jobs_otodom()
+                old_thread = threads_dict['process_defined_jobs_otodom']
+                if old_thread and old_thread.is_alive():
+                    old_thread.join(1)
+                threads_dict['process_defined_jobs_otodom'] = new_thread
+            elif 'process_scheduled_jobs' in name:
+                new_thread = process_defined_jobs()
+                old_thread = threads_dict['process_defined_jobs']
+                if old_thread and old_thread.is_alive():
+                    old_thread.join(1)
+                threads_dict['process_defined_jobs'] = new_thread
+
+            new_thread.start()
+            print(f'Thread {name} is dead. New one was reborn')
 
 
 def run_extra_threads():
     threads = {}
-    defined_jobs_thread = defined_jobs()
-    defined_jobs_thread_otodom = defined_jobs_otodom()
 
-    threads['defined_jobs'] = defined_jobs_thread
-    threads['defined_jobs_otodom'] = defined_jobs_thread_otodom
+    threads['defined_jobs'] = defined_jobs()
+    threads['defined_jobs_otodom'] = defined_jobs_otodom()
 
-    process_defined_jobs_thread = process_defined_jobs()
-    process_defined_jobs_thread_otodom = process_defined_jobs_otodom()
-
-    threads['process_defined_jobs'] = process_defined_jobs_thread
-    threads['process_defined_jobs_otodom'] = process_defined_jobs_thread_otodom
+    threads['process_defined_jobs'] = process_defined_jobs()
+    threads['process_defined_jobs_otodom'] = process_defined_jobs_otodom()
 
     for i in range(8):
         threads[f'process_single_offer_{i}'] = process_single_offer()
@@ -104,3 +144,5 @@ def run_extra_threads():
 
 if __name__ == '__main__':
     run_extra_threads()
+
+
