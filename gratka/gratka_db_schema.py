eafd6623416af2aff32d93196d9a948592bb9797
Dawid Mazur <dawmaz@gmail.com>
2024-01-29 00:06:07 +0100

    Last one before docker build

--- gratka/gratka_db_schema.py
+++ gratka/gratka_db_schema.py
@@ -3,7 +3,7 @@ from contextlib import contextmanager
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker, scoped_session
 
-DATABASE_URL = 'postgresql://postgres:02589qwert@localhost:5432/postgres'
+DATABASE_URL = 'postgresql://postgres:02589qwert;!/A@localhost:5432/postgres'
 engine = create_engine(DATABASE_URL)
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
@@ -100,7 +100,7 @@ class LoggedError(Base):
     process_name = Column(String)
     value = Column(String)
     date = Column(DateTime)
-    error_message = Column(String)
+    error_message = Column(String(98000))
 
 
 
